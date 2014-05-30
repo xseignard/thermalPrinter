d257338adacd4bef3fe41d3bd79f5fea3a4cd5db
xseignard <xavier.seignard@gmail.com>
2014-05-30 19:32:19 +0200

    Added a tweetwall demo

---
+++ demo/tweetPrint.js
@@ -0,0 +1,34 @@
+var SerialPort = require('serialport').SerialPort,
+	serialPort = new SerialPort('/dev/ttyUSB0', {
+		baudrate: 19200
+	}),
+	Printer = require('../src/printer'),
+	twitter = require('ntwitter');
+
+var creds = {
+	consumer_key: '1B5KBUELhN2NY1zuDs0xxyO8p',
+	consumer_secret: 'y6XMwwVVTC3CUaZAEYm1DZWa3FF3XVnKNRXLWlvEA0aEFAeWyn',
+	access_token_key: '102975831-9JzJ4wuYfXyUuEww1N6UHUFdsl1W4jkqtgSIsLvr',
+	access_token_secret: 'XnD4yRh6g95FidbRrMZWvJmuG0nH8B06uMytL2EYdrFEE'
+};
+
+var twitterClient = new twitter({
+	consumer_key: creds.consumer_key,
+	consumer_secret: creds.consumer_secret,
+	access_token_key: creds.access_token_key,
+	access_token_secret: creds.access_token_secret
+});
+
+
+serialPort.on('open',function() {
+	var printer = new Printer(serialPort);
+	printer.on('ready', function() {
+		twitterClient.stream('statuses/filter', {track: ['#nodejs']}, function(stream) {
+			stream.on('data', function (data) {
+				printer.printLine(data.text).lineFeed(3).print(function() {
+					console.log(data);
+				});
+			});
+		});
+	});
+});
