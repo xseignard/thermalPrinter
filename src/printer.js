f04cadf618704f77d9eb956374ce5e203c80827b
Flurin Egger <flurin@digitpaint.nl>
2014-09-14 01:12:25 +0200

    Add code for barcode printing (missing tests)

--- src/printer.js
+++ src/printer.js
@@ -269,5 +269,108 @@ Printer.prototype.barcodeHeight = function(h){
   return this.writeCommands(commands);
 }
 
+Printer.BARCODE_CHARSETS = {
+  NUMS: function(n){ return n >= 48 && n <= 57 },
+  ASCII: function(n){ return n >= 0 && n <= 127 }
+}
+
+Printer.BARCODE_TYPES = {
+  UPCA : {
+    code: 65,
+    size: function(n){ return n == 11 || n == 12; },
+    chars: Printer.BARCODE_CHARSETS.NUMS
+  },
+  UPCE : {
+    code: 66,
+    size: function(n){ return n == 11 || n == 12; },
+    chars: Printer.BARCODE_CHARSETS.NUMS
+  },
+  EAN13 : {
+    code: 67,
+    size: function(n){ return n == 12 || n == 13; },
+    chars: Printer.BARCODE_CHARSETS.NUMS
+  },
+  EAN8 : {
+    code: 68,
+    size: function(n){ return n == 7 || n == 8; },
+    chars: Printer.BARCODE_CHARSETS.NUMS
+  },
+  CODE39 : {
+    code: 69,
+    size: function(n){ return n > 1; },
+    chars: function(n){ 
+      // " $%+0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      return (
+        n == 32 ||
+        n == 36 ||
+        n == 37 ||
+        n == 43 ||
+        (n >= 45 && n <= 57) ||
+        (n >= 65 && n <= 90)
+      )
+     } 
+  },
+  I25 : {
+    code: 70,
+    size: function(n){ return n > 1 && n % 2 == 0; },
+    chars: Printer.BARCODE_CHARSETS.NUMS
+  },
+  CODEBAR : {
+    code: 71,
+    size: function(n){ return n > 1; },
+    chars: function(n){ 
+      return (
+        n == 36 ||
+        n == 43 ||
+        (n >= 45 && n <= 58) ||
+        (n >= 65 && n <= 68)
+      )
+     } 
+  },
+  CODE93 : {
+    code: 72,
+    size: function(n){ return n > 1; },
+    chars: Printer.BARCODE_CHARSETS.ASCII
+  },
+  CODE128 : {
+    code: 73,
+    size: function(n){ return n > 1; },
+    chars: Printer.BARCODE_CHARSETS.ASCII
+  },
+  CODE11 : {
+    code: 74,
+    size: function(n){ return n > 1; },
+    chars: Printer.BARCODE_CHARSETS.NUMS
+  },
+  MSI : {
+    code: 75,
+    size: function(n){ return n > 1; },
+    chars: Printer.BARCODE_CHARSETS.NUMS
+  }
+}
+
+Printer.prototype.barcode = function(type, data){
+  var commands = [29, 107];
+  commands.push(type.code);
+  commands.push(data.length);
+
+  // Validate size
+  if(!type.size(data.length)){
+    return this;
+  };
+
+  for(var i=0; i < data.length; i++){
+    var code = data.charCodeAt(i);  
+    if(!type.chars(code)){
+      return this;
+    }
+
+    commands.push(code);
+  }
+
+  return this.writeCommands(commands);
+}
+
+
 
 module.exports = Printer;
