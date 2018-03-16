04d276b276176e05b24e62b8075d2c5e0d32a141
Paul Hayes <paul.hayes@entropedia.co.uk>
2018-03-16 13:26:24 +0000

    HasPaper demo was broken

--- demo/testHasPaper.js
+++ demo/testHasPaper.js
@@ -1,8 +1,15 @@
 var SerialPort = require('serialport'),
 	serialPort = new SerialPort('/dev/ttyUSB0', {
-		baudrate: 19200
+		baudRate: 19200
 	}),
 	Printer = require('../src/printer');
+
+var opts = {
+    maxPrintingDots: 15,
+    heatingTime: 150,
+    heatingInterval: 4,
+    commandDelay: 5
+};
 
 serialPort.on('open',function() {
 	var printer = new Printer(serialPort, opts);
